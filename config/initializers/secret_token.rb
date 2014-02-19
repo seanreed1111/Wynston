03bd5e81fae17dc9492bbf81a7067ece83569262
Sean Reed <seanreed1111@gmail.com>
2014-02-19 12:31:51 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wynston::Application.config.secret_token = 'c64f11df9a768f0c7dcac171d3a4d7cd760223e529631fe97983150f06b47ed3fa1d7e073782370a37f632330e6c75bbd4638160b3b68f382258f6a850459ccb'
