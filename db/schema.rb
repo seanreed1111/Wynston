718b42ce49176d836b0ac46f255599afd31911b4
fdw101 <felicia.ogarro@gmail.com>
2014-02-20 10:29:48 -0500

    pull from origin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,94 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140219175201) do
+ActiveRecord::Schema.define(:version => 20140219224513) do
+
+  create_table "answers", :force => true do |t|
+    t.integer  "choice_id"
+    t.integer  "student_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "assessments", :force => true do |t|
+    t.integer  "grade"
+    t.integer  "course_module_id"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
+
+  create_table "choices", :force => true do |t|
+    t.text     "answer"
+    t.boolean  "correct"
+    t.integer  "question_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "course_modules", :force => true do |t|
+    t.string   "title"
+    t.integer  "course_id"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "course_professors", :force => true do |t|
+    t.integer  "course_id"
+    t.integer  "professor_id"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
+
+  create_table "courses", :force => true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "intro_video"
+    t.text     "syllabus"
+    t.string   "language"
+    t.integer  "hours"
+    t.text     "prerequisites"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.string   "image_url"
+    t.integer  "professor_id"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
+  create_table "enrollments", :force => true do |t|
+    t.date     "date_enrolled"
+    t.integer  "final_grade"
+    t.boolean  "certification"
+    t.date     "date_completed"
+    t.integer  "student_id"
+    t.integer  "course_id"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
+
+  create_table "institutions", :force => true do |t|
+    t.string   "name"
+    t.integer  "professor_id"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
+
+  create_table "lectures", :force => true do |t|
+    t.text     "note"
+    t.string   "note_url"
+    t.string   "video_url"
+    t.integer  "course_module_id"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
+
+  create_table "questions", :force => true do |t|
+    t.text     "question_text"
+    t.integer  "assessment_id"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
@@ -20,7 +107,7 @@ ActiveRecord::Schema.define(:version => 20140219175201) do
     t.string   "image_url"
     t.string   "password_digest"
     t.string   "about_me"
-    t.string   "user_flag"
+    t.string   "type"
     t.string   "course_register"
     t.string   "phone"
     t.string   "institution"
