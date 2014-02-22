7f117a8674a00344e444772e21125476e8c1b8b2
fdw101 <felicia.ogarro@gmail.com>
2014-02-22 10:09:18 -0500

    add links, change root route and update course index layout

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Wynston::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => redirect('/users')
- 
+   root :to => 'static_pages#home'
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
