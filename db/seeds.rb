f74e6177ca010684cdd3bfcc1b07e7d757f0c72e
Sean Reed <seanreed1111@gmail.com>
2014-02-25 15:03:15 -0500

    changes to proper has_many through: syntax. Adds further data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,50 +1,48 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-#also, note that this is a ruby file, so you can also do the following:
-# #country_list = [
-#   [ "Germany", 81831000 ],
-#   [ "France", 65447374 ],
-#   [ "Belgium", 10839905 ],
-#   [ "Netherlands", 16680000 ]
-# ]
-
-# country_list.each do |name, population|
-#   Country.create( name: name, population: population )
-# end
 
 
-# course_list = [
-#   ["Intro to Travel, Tourism & Hospitality Management", "Introduction to the world of Travel and Tourism", "None"],
-#   ["Business Statistics I: MAT 120", "Statistical concepts that ever entrepreneur needs to know.", "Precalculus or MAT 200"],
-#   ["Introduction to Information Systems: BTC 200","Introduction to the ways computers are used in a modern business environment","None"],
-#   ["Entrepreneurial Finance: BTM 252","A deep dive into some of the common financial pitfalls faced by startups and how to avoid them.","Introduction to Entrepreneurship: BTM 100"],
-#   ["Introduction to Computer Programming","Learn the logic of how to write your own programs on a computer.","None"],
-#   ["Introduction to Web Design","Learn how HTML, CSS, and Javascript are used to build websites.","None"],
-#   ["Introduction to Entrepreneurship","So you want to be an entrepreneur? Get started here!","None"]
-# ]
 
-# course_list.each do |title, description, prerequisites|
-#   Course.create(title: title, description: description, prerequisites: prerequisites)
-# end
+course_list = [
+  ["Intro to Travel, Tourism & Hospitality Management", "Introduction to the world of Travel and Tourism", "None"],
+  ["Business Statistics I: MAT 120", "Statistical concepts that ever entrepreneur needs to know.", "Precalculus or MAT 200"],
+  ["Introduction to Information Systems: BTC 200","Introduction to the ways computers are used in a modern business environment","None"],
+  ["Entrepreneurial Finance: BTM 252","A deep dive into some of the common financial pitfalls faced by startups and how to avoid them.","Introduction to Entrepreneurship: BTM 100"],
+  ["Introduction to Computer Programming","Learn the logic of how to write your own programs on a computer.","None"],
+  ["Introduction to Web Design","Learn how HTML, CSS, and Javascript are used to build websites.","None"],
+  ["Introduction to Entrepreneurship","So you want to be an entrepreneur? Get started here!","None"]
+]
 
-# 5.times {Professor.create(first_name: Faker::Name.first_name, 
-#                           last_name: Faker::Name.last_name, 
-#                           email: Faker::Internet.email, 
-#                           password: "12345678") 
-# }
+course_list.each do |title, description, prerequisites|
+  Course.create(title: title, description: description, prerequisites: prerequisites)
+end
 
-# 5.times {Student.create(first_name: Faker::Name.first_name, 
-#                           last_name: Faker::Name.last_name, 
-#                           email: Faker::Internet.email, 
-#                           password: "12345678") 
-# }
+5.times {Professor.create(first_name: Faker::Name.first_name, 
+                          last_name: Faker::Name.last_name, 
+                          email: Faker::Internet.email, 
+                          password: "12345678") 
+}
+
+5.times {Student.create(first_name: Faker::Name.first_name, 
+                          last_name: Faker::Name.last_name, 
+                          email: Faker::Internet.email, 
+                          password: "12345678") 
+}
 
 
 # 5.times {Institution.create(name: Faker::Company.name + " University")}
+5.times do |n| 
+  CourseModule.create(title: "Module #{n}")
+end
+
+5.times do |n|
+  Assessment.create(grade: n*16)
+end
+
+Question.create(question_text: "What is the capital of Pennsylvania?" )
+Question.create(question_text: "What is the capital of North Dakota" )
+Question.create(question_text: "What is the capital of South Dakota?" )
+Question.create(question_text: "What is the capital of Kansas?" )
+Question.create(question_text: "What is the capital of Idaho?" )
 
