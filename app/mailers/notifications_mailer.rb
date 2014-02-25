fb298a349992387ef986e742e6db00d99218eafb
LewisJPaul <lewis.jp@gmail.com>
2014-02-25 10:35:26 -0500

    email updated

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,7 +1,7 @@
 class NotificationsMailer < ActionMailer::Base
 
   default :from => "testing25chapel@gmail.com"
-  default :to => "testing25chapel@gmail.com"
+  default :to => "dreamyamine@gmail.com"
 
   def new_message(message)
   	@message = message
