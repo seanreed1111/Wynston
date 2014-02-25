f97c2707cc88b784c00de0ef5bc67b937a664c9b
Amine TheDream <atourki@gmail.com>
2014-02-25 11:56:20 -0500

    add video functionality

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'rails', '3.2.16'
 
 gem 'devise' 
 gem 'haml'
+gem 'paperclip'
+gem 'debugger'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,4 +4,22 @@ class CoursesController < ApplicationController
 		@courses = Course.all
 	end 
 
+  def create
+  @course = Course.new(params[:course])
+
+	  respond_to do |format|
+		    if @course.save
+		      format.html { redirect_to @course, notice: 'Course was successfully created.' }
+		      format.json { render json: @course, status: :created, location: @course }
+		    else
+		      format.html { render action: "new" }
+		      format.json { render json: @course.errors, status: :unprocessable_entity }
+		    end
+	  end
+	end
+
+	def video_path
+		@videopath = @course.video.path.match(/\/system(.*)/)
+	end
+
 end
