c711818983e13f560657959d2a4689a56f880ae0
fdw101 <felicia.ogarro@gmail.com>
2014-02-26 18:18:19 -0500

    update course_guide

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -3,13 +3,15 @@ class AssessmentsController < ApplicationController
 	def new
 		@assessment = Assessment.new
 		3.times do
-			@assessment.questions.build
+		assessments	= @assessment.questions.build
+			4.times{assessments.choices.build}
+			
 		end
 	end
 
 	def create
 		# debugger
-		Choices.new(params[:questions][:choices])
+		# Choices.new(params[:questions][:choices])
 
 	end
 end
