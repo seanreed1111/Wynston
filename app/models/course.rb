3257bb7809181bad4561960850d841d722f9bbf2
Sean Reed <seanreed1111@gmail.com>
2014-02-26 17:36:33 -0500

    before merge with branch capulet

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,10 +1,10 @@
 class Course < ActiveRecord::Base
 	attr_accessible :title, :description, :intro_video, :syllabus, :language,
    :hours, :prerequisites, :start_date, :end_date, :image_url, :professor_id, 
-   :video, :about 
+   :video, :about
 	has_attached_file :video
 	has_many :assessments
-	has_many :professors
+	belongs_to :professor
 	has_many :enrollments
   validates_attachment_content_type :video, :content_type => /.*/
 
--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,5 +1,4 @@
 class Professor < User
     belongs_to :institution
     has_many :courses
-
 end
