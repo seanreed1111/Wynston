ed7025f6de74bc432fafa0e9ae098e20e657faa0
LewisJPaul <lewis.jp@gmail.com>
2014-02-27 15:31:09 -0500

    Basic assessment views

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -11,6 +11,39 @@ class AssessmentsController < ApplicationController
 	end
 
 
+	def index 
+		@assessments = Assessment.all
+	end 
+
+	def show
+		@assessment = Assessment.find(params[:id])
+	end 
+
+	def create
+  	@assessment = Assessment.new(params[:course])
+
+	  respond_to do |format|
+		    if @assessment.save
+		      format.html { redirect_to @assessment, notice: 'Assessment was successfully created.' }
+		      format.json { render json: @assessment, status: :created, location: @course }
+		    else
+		      format.html { render action: "new" }
+		      format.json { render json: @assessment.errors, status: :unprocessable_entity }
+		    end
+	  end
+	end
+
+	# def video_path
+	# 	@videopath = @course.video.path.match(/\/system(.*)/)
+	# end
+
+
+
+
+	def edit
+		@assessment = Assessment.find(params[:id])
+	end 
+
 	# def create
 		# debugger
 		# Choices.new(params[:questions][:choices])
