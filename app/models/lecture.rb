e250d0b6d0082a1475b2749cf95156ce7c601419
Sean Reed <seanreed1111@gmail.com>
2014-03-11 15:46:50 -0400

    changed typo in lecture.rb

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,7 +1,4 @@
 class Lecture < ActiveRecord::Base
 	attr_accessible :note, :note_url, :video_url
-
-    belongs_to :course_module
-
-    attr_accessible :note, :note_url, :video_url
+  belongs_to :course_module
 end
